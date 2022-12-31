5f45957e823bd5e50ae69745dbdaaa27b5999439
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-12-31 11:54:28 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>DOM Starter</h1>`;
