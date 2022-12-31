790a6bc19274e2b0dfd350ed23a2e5182fa2ef7e
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-12-31 13:08:04 +0530

    2 files modified

--- index.js
+++ index.js
@@ -44,6 +44,22 @@ function createDiv(className = null, id = null) {
   return div;
 }
 
+function createParagraph(
+  className = null,
+  id = null,
+  content = '',
+  flag = 'asText'
+) {
+  const p = document.createElement('p');
+  if (className) p.setAttribute('class', className);
+  if (id) p.setAttribute('id', id);
+  if (content) {
+    if (flag === 'asText') p.innerText = content;
+    if (flag === 'asHTML') p.innerHTML = content;
+  }
+  return p;
+}
+
 function createImg(
   className = null,
   id = null,
@@ -65,12 +81,60 @@ const imgSrc =
   'https://images.pexels.com/photos/449520/pexels-photo-449520.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=75&amp;w=75';
 
 const createContainer = createDiv('container', 'container');
-const createCard = createDiv('card');
+const createCard = createDiv('card border-primary bg-light');
 const createCardBody = createDiv('card-body');
 const createImgCard = createImg('card-img-top', null, '100%', 175, imgSrc);
+const para = createParagraph(null, null, 'Hello world', 'asText');
+let intervalCapture = 0;
+let timeOutCapture = 0;
+// one way
+// const data = para.innerHTML;
+// const dataSplit = data.split(' ');
+// let temp = dataSplit.map((item, index) => {
+//   if (index === dataSplit.length - 1)
+//     return `<span class="bg-primary p-2">${item}</span>`;
+//   else return item;
+// });
+
+// para.innerHTML = temp.join('');
+
+// other way
+
+const para2 = createParagraph(
+  null,
+  'dynamicContructedPara',
+  'Hello <span class="bg-primary p-2">World</span>',
+  'asHTML'
+);
+
+createCardBody.appendChild(createDiv().appendChild(para2));
+
+// // update
+// intervalCapture = setInterval(() => {
+//   console.log(para2);
+//   para2.innerHTML = new Date().getTime();
+// }, 2 * 1000);
+
+// timeOutCapture = setTimeout(() => {
+//   clearInterval(intervalCapture);
+//   document.querySelector('#dynamicContructedPara').innerHTML = '';
+// }, 10 * 1000);
 
 createCard.appendChild(createImgCard);
 createCard.appendChild(createCardBody);
 createContainer.appendChild(createCard);
-
+createContainer.setAttribute('style', 'display:none');
 appDiv.appendChild(createContainer);
+
+// setTimeout(() => {
+//   console.log('executed');
+
+//   // to remove child from existing node
+//   appDiv.removeChild(document.querySelector('#container'));
+// }, 20 * 1000);
+
+document.addEventListener('DOMCONTENTLoaded', () => {
+  // console.clear();
+  console.log('input', document.querySelector('input#validationServer01'));
+  //.addEventListener('change', (e) => console.log);
+});
