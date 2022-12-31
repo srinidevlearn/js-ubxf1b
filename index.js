0ea041e7707b90b613f67a32a2957efe69ab9d00
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-12-31 12:00:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>DOM Starter</h1>`;
+
+const containerClass = document.getElementsByClassName('container');
+
+console.log(containerClass);
+
+const containerById = document.getElementById('container');
+console.log(containerById);
+
+const selectAllDiv = document.getElementsByTagName('div');
+console.log(selectAllDiv);
+
+//Why powerfull ?
+
+const powerFullSelector = document.querySelector(
+  'div#container  div.card-body img'
+);
+console.log(powerFullSelector);
+
+const powerFullSelectorOnWhole = document.querySelectorAll('div');
+console.log(powerFullSelectorOnWhole);
